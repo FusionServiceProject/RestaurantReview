92c3ba669330dc366292a897d5021e468ae65a33
sukyeongh <hsk10271@gmail.com>
2021-12-14 19:45:32 +0900

    Revert "commit"
    This reverts commit d3395bc9e8eca808ca0b78fb600bde82e24b5dea.

--- ReviewService/src/main/java/restaurant/orderManagement/service/OrderListService.java
+++
@@ -1,5 +0,0 @@
-package restaurant.orderManagement.service;
-
-public interface OrderListService {
-	String insertOrderList(OrderListVO vo) throws Exception;
-}
--- ReviewService/src/main/java/restaurant/orderManagement/service/ShoppingBagService.java
+++
@@ -1,5 +0,0 @@
-package restaurant.orderManagement.service;
-
-public interface ShoppingBagService {
-	void deleteShoppingBag(ShoppingBagVO vo) throws Exception;
-}
